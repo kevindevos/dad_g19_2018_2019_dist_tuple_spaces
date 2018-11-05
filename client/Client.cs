6b15f774951d6d94f07ae1d2e8fdb4b7d27a1ada
simaovc <simaovalecardoso@gmail.com>
2018-11-05 22:46:45 +0000

    Revert "client script implemented. still need client.methods to return tuples on read and take."
    This reverts commit 73a2c0d

--- client/Client.cs
+++ client/Client.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using Tuple = CommonTypes.Tuple;
 
 namespace ClientNamespace {
-    public class Client : RemotingEndpoint, IRemoting {
+    class Client : RemotingEndpoint, IRemoting {
 
         private int clientRequestSeqNumber;
         public int ClientRequestSeqNumber { get; set; }
