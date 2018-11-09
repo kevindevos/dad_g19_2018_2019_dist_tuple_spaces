a078fb6c89bdf2a1a52a632e007ca852798599f4
Kevin <kevindevos9542@gmail.com>
2018-11-09 16:43:17 +0000

    Client XL write

--- client/ClientXL.cs
+++ client/ClientXL.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommonTypes;
 using CommonTypes.message;
 using Tuple = CommonTypes.tuple.Tuple;
 
 namespace ClientNamespace {
     public class ClientXL : Client {
+        public List<RemotingEndpoint> View { get { return KnownServerRemotes; } } // change name for XL
+
         public ClientXL() : this(DefaultClientHost, DefaultClientPort) {
         }
 
@@ -16,7 +19,16 @@ namespace ClientNamespace {
 
        
         public override void Write(Tuple tuple) {
-            
+            // TODO remote exceptions?
+            var request = new Request(ClientRequestSeqNumber, EndpointURL, RequestType.WRITE, tuple);
+
+            Console.WriteLine("Sending request with sequence number: " + request.SeqNum);
+            SendMessageToView(request);
+            ClientRequestSeqNumber++;
+        }
+
+        private void SendMessageToView(Request request) {
+            SendMessageToRemotes(KnownServerRemotes, request);
         }
 
         public override Tuple Read(Tuple tuple) {
