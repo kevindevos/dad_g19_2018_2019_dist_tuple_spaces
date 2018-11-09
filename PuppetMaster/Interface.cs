6690dd0489316bff8eee109c2b85cd2128071a44
fchamicapereira <fchamicapereira@gmail.com>
2018-11-09 14:03:26 +0000

    progress on puppet master

--- PuppetMaster/Interface.cs
+++ PuppetMaster/Interface.cs
@@ -9,24 +9,47 @@ namespace PuppetMaster
 {
     class Interface
     {
-        private readonly string PCS_ADDR_PATH = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/TextFile1.txt";
-        private string[] pcs_addrs;
-
+        private readonly string PCS_ADDR_PATH = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/pcs_addrs.txt";
         private Controller controller;
 
         public Interface()
         {
             Console.WriteLine("===== Welcome to the Puppet Master =====");
-            pcs_addrs = File.ReadAllLines(@PCS_ADDR_PATH);
+
+            string[] pcs_addrs = File.ReadAllLines(@PCS_ADDR_PATH);
+            controller = new Controller(pcs_addrs);
         }
 
-        private string[] GetPCS()
+        public void Start()
         {
-            
-            Console.ReadLine();
-
-            return lines;
 
         }
+
+        private void Help()
+        {
+            int value = -1;
+            string response;
+
+            Console.WriteLine("1. Read configuration script");
+            Console.WriteLine("2. Manually input commands");
+
+            while (value != 1 && value != 2)
+            {
+                Console.Write("\n[1|2] ");
+                response = Console.ReadLine();
+                Int32.TryParse(response, out value);
+            }
+        }
+
+        private void ReadScript()
+        {
+
+        }
+
+        private void ParseCommand()
+        {
+
+        }
+
     }
 }
