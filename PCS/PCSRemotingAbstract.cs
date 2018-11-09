324b9821d4035e77426d3b82b36c932afb6621b1
Francisco Pereira <fchamicapereira@gmail.com>
2018-11-09 20:59:54 +0000

    fixed bugs and added TODOs

--- PCS/PCSRemotingAbstract.cs
+++ PCS/PCSRemotingAbstract.cs
@@ -8,28 +8,16 @@ namespace PCS
 {
     abstract class PCSRemotingAbstract : PCSRemotingInterface
     {
-        protected int clientPortCounter;
-        protected int serverPortCounter;
-
         protected readonly Dictionary<string, Client> clients;
         protected readonly Dictionary<string, Server> servers;
 
         public PCSRemotingAbstract()
         {
-            clientPortCounter = 20000;
-            serverPortCounter = 40000;
-
             clients = new Dictionary<string, Client>();
             servers = new Dictionary<string, Server>();
         }
 
-        public void Client(string client_id, string URL, string[] script)
-        {
-            Client client = new Client(URL, clientPortCounter);
-            clients.Add(client_id, client);
-            clientPortCounter++;
-        }
-
+        public abstract void Client(string client_id, string URL, string[] script);
         public abstract void Server(string server_id, string URL, int min_delay, int max_delay);
     }
 }
--- PCS/PCSRemotingXL.cs
+++ PCS/PCSRemotingXL.cs
@@ -1,14 +1,25 @@
-﻿using ServerNamespace;
+﻿using ClientNamespace;
+using ServerNamespace;
 
 namespace PCS
 {
     class PCSRemotingXL : PCSRemotingAbstract
     {
+        public override void Client(string client_id, string URL, string[] script)
+        {
+            // TODO add new constructor to accept URL instead of host and port
+            // TODO add script functionality
+
+            // Client client = new ClientSMR(URL);
+            // clients.Add(client_id, client);
+        }
+
         override public void Server(string server_id, string URL, int min_delay, int max_delay)
         {
-            Server server = new ServerXL(URL, serverPortCounter);
-            servers.Add(server_id, server);
-            serverPortCounter++;
+            // TODO add new constructor to accept URL instead of host and port
+
+            // Server server = new ServerXL(URL);
+            // servers.Add(server_id, server);
         }
     }
 }
